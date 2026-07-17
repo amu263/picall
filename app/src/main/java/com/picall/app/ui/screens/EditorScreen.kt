cc28891fcdc8b1913df9dd8bbe8bd80fcf7b3495
amu263 <169724152zmljyn@gmail.com>
2026-07-17 23:03:54 +0800

    fix: add TextOverflow import

--- app/src/main/java/com/picall/app/ui/screens/EditorScreen.kt
+++ app/src/main/java/com/picall/app/ui/screens/EditorScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
